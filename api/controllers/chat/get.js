1e55d3152b328cf8b30ba044a8b91909c685ecde
Aman Arya <aman.arya@dal.ca>
2019-04-02 11:58:46 -0300

    Bug fix: Group addition

--- api/controllers/chat/get.js
+++ api/controllers/chat/get.js
@@ -25,6 +25,9 @@ module.exports = {
 
     fn: async function (inputs, exits) {
         let group = await Groups.findOne({ itinerary: inputs.itineraryId });
+        if (!group) {
+            return;
+        }
         let messages = await Chat.find({ group: group.id }).sort('createdAt DESC');
         for (i = 0; i < messages.length; i += 1) {
             let user = await User.findOne({ id: messages[i].sender });
