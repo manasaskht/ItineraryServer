472a760176a6c8a6701ff620499ee7cf139e60fe
Nikhil Tyagi <nikhil.tyagi62@gmail.com>
2019-04-10 23:01:25 -0300

    Update datastores.js

--- config/datastores.js
+++ config/datastores.js
@@ -51,8 +51,8 @@ module.exports.datastores = {
         // adapter: 'sails-mysql',
         // url: 'mysql://user:password@host:port/database',
 
-        adapter: 'sails-mongo',
-        url: 'mongodb://localhost:27017/itinerary'
+        // adapter: 'sails-mongo',
+        // url: 'mongodb://localhost:27017/itinerary'
 
     },
 
